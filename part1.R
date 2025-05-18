6b11c6ce97a8421a6fc7b5207d94c29be6f0fe3b
Matej Vrecar <matej@vrecar.si>
2025-05-18 19:59:10 +0200

    initial commit

---
+++ part1.R
@@ -0,0 +1,43 @@
+library(mc2d)
+
+a = 0
+b = 10
+c = 100
+
+Ex = (a + 4*b + c)/6
+
+pert <- function(x, a=0, b=10, c=100){
+  alpha = 1 + 4*((b-a)/(c-a))
+  bet = 1 + 4*((c-b)/(c-a))
+  
+  numerator = ((x - a) ^ (alpha-1)) * ((c-x)^(bet-1))
+  denominator = beta(alpha, bet)*((c-a)^(alpha+bet-1))
+  
+  return(numerator/denominator)
+}
+
+trapezoidal_rule_for_expected_value <- function(lower_bound, upper_bound, fun, N){
+  
+  cum_sum <- 0
+  h = (upper_bound - lower_bound) / N
+  
+  for (x in seq(lower_bound, upper_bound, h)){
+    fx <- x * fun(x)
+    if (x == lower_bound | x == upper_bound){
+      cum_sum <- cum_sum + fx
+    }
+    else{
+      cum_sum <- cum_sum + 2*fx
+    }
+  }
+  return((h / 2)*cum_sum)
+}
+
+#It takes 302 evaluations to get the value approximated to 4 decimal places
+trapezoidal_rule_for_expected_value(a, c, pert, 302)
+
+n = 40000
+xs = runif(n, a, c)
+prob = 1/c-a
+
+1/(n * prob) * sum(xs * pert(xs))
