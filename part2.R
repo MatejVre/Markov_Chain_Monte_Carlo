be8f637d2ca95c2ba636f17334c969134426ce11
Matej Vrecar <matej@vrecar.si>
2025-05-18 20:35:03 +0200

    forgot to save :)

--- part2.R
+++ part2.R
@@ -1,3 +1,35 @@
 integrand <- function(x){
-  (x ^ (-3/4)) * exp(1)
+  (x ^ (-3/4)) * exp(-x)
 } 
+
+linspan = seq(0, 1, 0.01)
+
+plot(linspan, integrand(linspan))
+
+#estimating the integral using uniform distribution
+
+n = 10^7
+prob = 1
+xs = runif(n, 0, 1)
+
+1/(n*prob) * sum(integrand(xs))
+
+#estimating the integral using inversion sampling
+pdf <- function(x){
+  return(1/4*x^(-3/4))
+}
+
+cdf <- function(x){
+  return(x^(1/4))
+}
+
+inverse_cdf <- function(x){
+  return(x^4)
+}
+
+xs_u <-  runif(n, 0, 1)
+xs_real <- inverse_cdf(xs_u)
+
+1/n * sum(integrand(xs_real)/pdf(xs_real))
+
+
